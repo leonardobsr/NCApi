caa1a0fe637dfbe8869c45fdcce92285e0f34f59
Anderson Lentz <anderlent@gmail.com>
2019-04-16 16:27:52 -0300

    Using search bar to request country from API

--- NCApi/ViewController.swift
+++ NCApi/ViewController.swift
@@ -13,19 +13,28 @@ class ViewController: UITableViewController {
     
     private let tablCellId = "countryCell"
     private let detailScreenId = "countryDetailsIdentifier"
-    
+    var searchController: UISearchController!
     var aryDownloadedData:[Country]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateRowsWithCountries(query: nil, page: 1, limit: 1)
+        
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = true
+        searchController.searchBar.placeholder = "Search here..."
+        searchController.searchBar.delegate = self
+        searchController.searchBar.sizeToFit()
+        tableView.tableHeaderView = searchController.searchBar
+        
+        
+        
     }
     
     func updateRowsWithCountries(query: String?, page: Int, limit: Int) {
         Response.getCountries(query: query, page: page, limit: limit, completion: { (data: Response?, error: Error?) in
             if let responseData = data {
                 self.aryDownloadedData = responseData.Response
-                
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
                 }
@@ -40,6 +49,7 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: tablCellId) as! CountryTableViewCell
         cell.countryNameLabel?.text = self.aryDownloadedData?[indexPath.row].Name
+
         return cell
     }
     
@@ -62,4 +72,38 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: self.detailScreenId, sender: self.aryDownloadedData?[indexPath.row])
     }
+}
+
+//MARK: - UISearchbar delegate
+extension ViewController: UISearchBarDelegate{
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        tableView.reloadData()
+    }
+    
+    
+}
+
+extension ViewController: UISearchResultsUpdating{
+    func updateSearchResults(for searchController: UISearchController) {
+        //self.aryDownloadedData?.removeAll(keepingCapacity: false)
+        if let searchText = searchController.searchBar.text,
+            !searchText.isEmpty {
+            print(searchText)
+            updateRowsWithCountries(query: searchText, page: 1, limit: 10)
+                        
+            /*
+            filteredCountryArray = aryDownloadedData?.filter {$0.Name?.lowercased().contains(searchText.lowercased()) ?? false} ?? []
+            
+            if filteredCountryArray.count == 0{
+                //Not found country than request API using search text
+                updateRowsWithCountries(query: searchText, page: 1, limit: 10)
+            }*/
+            
+            tableView.reloadData()
+        }
+    }
+    
+    
+    
+    
 }
