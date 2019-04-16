a634cb278cd2f2a73a6d89879c3d3cc2d3b3b17c
Leonardo Reis <leonardobsr@gmail.com>
2019-04-16 17:08:31 -0300

    update

--- NCApi/DetailViewController.swift
+++ NCApi/DetailViewController.swift
@@ -13,12 +13,50 @@ class DetailViewController: UIViewController {
     var country: Country?
     @IBOutlet weak var flagImageView: UIImageView!
     @IBOutlet weak var countryName: UILabel!
+    @IBOutlet weak var subRegion: UILabel!
+    @IBOutlet weak var currencyName: UILabel!
+    
+    @IBOutlet weak var region: UILabel!
+    @IBOutlet weak var nativeName: UILabel!
+    @IBOutlet weak var alpha3Code: UILabel!
+    @IBOutlet weak var latitude: UILabel!
+    @IBOutlet weak var longitude: UILabel!
+    @IBOutlet weak var area: UILabel!
+    @IBOutlet weak var numericCode: UILabel!
+    @IBOutlet weak var nativeLanguage: UILabel!
+    @IBOutlet weak var currencyCode: UILabel!
+    @IBOutlet weak var currencySymbol: UILabel!
+    @IBOutlet weak var alpha2Code: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        self.countryName.text = self.country?.Name
         self.flagImageView.imageFromServerURL(self.country?.FlagPng ?? "", placeHolder: nil)
+        self.countryName.text = self.country?.Name
+        self.countryName.text = self.country?.Name
+        
+        self.nativeName.text = self.country?.NativeName
+        self.nativeLanguage.text = self.country?.Name
+        
+        self.alpha2Code.text = self.country?.Alpha2Code
+        self.alpha3Code.text = self.country?.Alpha3Code
+        
+        if let nc = self.country?.NumericCode {
+            self.numericCode.text = String(nc)
+        }
+        
+        self.region.text = self.country?.Region
+        self.subRegion.text = self.country?.SubRegion
+        self.latitude.text = self.country?.Latitude
+        self.longitude.text = self.country?.Longitude
+        
+        if let ar = self.country?.Area {
+            self.area.text = String(ar)
+        }
+        
+        self.currencyName.text = self.country?.CurrencyName
+        self.currencyCode.text = self.country?.CurrencyCode
+        self.currencySymbol.text = self.country?.CurrencySymbol
     }
 }
--- NCApi/ViewController.swift
+++ NCApi/ViewController.swift
@@ -14,23 +14,65 @@ class ViewController: UITableViewController {
     private let tablCellId = "countryCell"
     private let detailScreenId = "countryDetailsIdentifier"
     
-    var aryDownloadedData:[Country]?
+    var isFetchInProgress: Bool = false
+    var totalCount: Int = 0
+    var page: Int = 1
+    var aryDownloadedData:[Country] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateRowsWithCountries(query: nil, page: 1, limit: 1)
+        self.tableView.prefetchDataSource = self
+        updateRowsWithCountries(query: nil, page: self.page, limit: 10)
+    }
+    
+    func onFetchCompleted(with newIndexPathsToReload: [IndexPath]?) {
+        // 1
+        guard let newIndexPathsToReload = newIndexPathsToReload else {
+//            self.indicatorView.stopAnimating()
+            tableView.isHidden = false
+            tableView.reloadData()
+            return
+        }
+        // 2
+        let indexPathsToReload = visibleIndexPathsToReload(intersecting: newIndexPathsToReload)
+        tableView.reloadRows(at: indexPathsToReload, with: .automatic)
     }
     
     func updateRowsWithCountries(query: String?, page: Int, limit: Int) {
+        guard !self.isFetchInProgress else {
+            return
+        }
+        self.isFetchInProgress = true
+        
         Response.getCountries(query: query, page: page, limit: limit, completion: { (data: Response?, error: Error?) in
             if let responseData = data {
-                self.aryDownloadedData = responseData.Response
-                
                 DispatchQueue.main.async {
+                    self.totalCount = responseData.TotalCount
+                    if (self.page < self.totalCount) {
+                        self.page += 1
+                    }
+                    self.aryDownloadedData += responseData.Response
+                    self.isFetchInProgress = false
                     self.tableView.reloadData()
                 }
             }
         })
+    }
+    
+    func calculateIndexPathsToReload(from newCountries: [Country]) -> [IndexPath] {
+        let startIndex = self.aryDownloadedData.count - newCountries.count
+        let endIndex = startIndex + newCountries.count
+        return (startIndex..<endIndex).map { IndexPath(row: $0, section: 0) }
+    }
+    
+    func isLoadingCell(for indexPath: IndexPath) -> Bool {
+        return indexPath.row >= self.totalCount
+    }
+    
+    func visibleIndexPathsToReload(intersecting indexPaths: [IndexPath]) -> [IndexPath] {
+        let indexPathsForVisibleRows = tableView.indexPathsForVisibleRows ?? []
+        let indexPathsIntersection = Set(indexPathsForVisibleRows).intersection(indexPaths)
+        return Array(indexPathsIntersection)
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -39,12 +81,18 @@ class ViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: tablCellId) as! CountryTableViewCell
-        cell.countryNameLabel?.text = self.aryDownloadedData?[indexPath.row].Name
+        cell.countryImageView?.imageFromServerURL(self.aryDownloadedData[indexPath.row].FlagPng ?? "", placeHolder: UIImage(named: "600X400"))
+        cell.countryNameLabel?.text = self.aryDownloadedData[indexPath.row].Name
+        if isLoadingCell(for: indexPath) {
+//            cell.configure(with: .none)
+        } else {
+//            cell.configure(with: self.aryDownloadedData(at: indexPath.row))
+        }
         return cell
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.aryDownloadedData?.count ?? 0
+        return self.aryDownloadedData.count
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -60,6 +108,14 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: self.detailScreenId, sender: self.aryDownloadedData?[indexPath.row])
+        performSegue(withIdentifier: self.detailScreenId, sender: self.aryDownloadedData[indexPath.row])
+    }
+}
+
+extension ViewController: UITableViewDataSourcePrefetching {
+    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
+        if indexPaths.contains(where: isLoadingCell) {
+            self.updateRowsWithCountries(query: nil, page: page, limit: 10)
+        }
     }
 }
