ca709195d349a81b8994e11b83d7fc4ba15516b5
Anderson Lentz <anderlent@gmail.com>
2019-04-16 17:07:45 -0300

    Country search working

--- NCApi/ViewController.swift
+++ NCApi/ViewController.swift
@@ -13,21 +13,21 @@ class ViewController: UITableViewController {
     
     private let tablCellId = "countryCell"
     private let detailScreenId = "countryDetailsIdentifier"
-    var searchController: UISearchController!
-    var aryDownloadedData:[Country]?
-
+    private var searchController: UISearchController!
+    private var aryDownloadedData:[Country]?
+    var spinner:UIActivityIndicatorView?
     override func viewDidLoad() {
         super.viewDidLoad()
         
         searchController = UISearchController(searchResultsController: nil)
         searchController.searchResultsUpdater = self
         searchController.dimsBackgroundDuringPresentation = true
-        searchController.searchBar.placeholder = "Search here..."
+        searchController.searchBar.placeholder = "Country name"
         searchController.searchBar.delegate = self
         searchController.searchBar.sizeToFit()
         tableView.tableHeaderView = searchController.searchBar
         
-        
+        spinner = makeSpinner(view: self.view)
         
     }
     
@@ -36,6 +36,7 @@ class ViewController: UITableViewController {
             if let responseData = data {
                 self.aryDownloadedData = responseData.Response
                 DispatchQueue.main.async {
+                    self.spinner?.dismissLoader()
                     self.tableView.reloadData()
                 }
             }
@@ -48,8 +49,7 @@ class ViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: tablCellId) as! CountryTableViewCell
-        cell.countryNameLabel?.text = self.aryDownloadedData?[indexPath.row].Name
-
+            cell.countryNameLabel?.text = self.aryDownloadedData?[indexPath.row].Name
         return cell
     }
     
@@ -72,6 +72,19 @@ class ViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: self.detailScreenId, sender: self.aryDownloadedData?[indexPath.row])
     }
+    
+    func makeSpinner(view: UIView) -> UIActivityIndicatorView {
+        
+        let spinner = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 70, height:70))
+        spinner.backgroundColor = UIColor.black
+        spinner.layer.cornerRadius = 3.0
+        spinner.clipsToBounds = true
+        spinner.hidesWhenStopped = true
+        spinner.style = UIActivityIndicatorView.Style.white;
+        spinner.center = view.center
+        view.addSubview(spinner)
+        return spinner
+    }
 }
 
 //MARK: - UISearchbar delegate
@@ -83,16 +96,22 @@ extension ViewController: UISearchBarDelegate{
     
 }
 
+//MARK: - UISearchResultsUpdating
 extension ViewController: UISearchResultsUpdating{
     func updateSearchResults(for searchController: UISearchController) {
         if let searchText = searchController.searchBar.text,
             !searchText.isEmpty {
+            self.spinner?.startAnimating()
             updateRowsWithCountries(query: searchText, page: 1, limit: 10)
             tableView.reloadData()
         }
     }
-    
-    
-    
-    
+}
+
+//MARK: - UIActivityIndicatorView
+extension UIActivityIndicatorView {
+    func dismissLoader() {
+        self.stopAnimating()
+        UIApplication.shared.endIgnoringInteractionEvents()
+    }
 }
